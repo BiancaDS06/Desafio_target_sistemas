708fd971643e790ea3db8b3296035843bafad222
Bianca Dias <97123676+BiancaDS06@users.noreply.github.com>
2024-03-05 23:25:07 -0300

    questão soma

--- 03/question.js
+++
@@ -1,9 +0,0 @@
-//3) Descubra a lógica e complete o próximo elemento:
-
-
-// a) 9. Sequência de números ímpares ou também pode ser + 2 (1+2=3; 3+2=5; 5+2=7; 7+2=9)
-// b) 128. Sempre multiplicando por 2 (2x2=4; 4x2=8; 8x2=16... 64x2=128)
-// c) 49. Soma em uma outra sequência de números ímpares (0+1=1, 1+3=4 ... 36+13=49)
-// d) 100. Números pares elevado ao quadrado(2², 4², ... 10²).
-// e) 13. Soma dos dois elementos anteriores: 1 (primeiro elemento), 1 (segundo elemento), 1+1=2, 1+2=3, 2+3=5, 3+5=8, 5+8=13.
-// f) 200. Baseado na letra inicial do número escrito por extenso, no caso 'd', como: dois, dez, doze, dezesseis, ... duzentos.
